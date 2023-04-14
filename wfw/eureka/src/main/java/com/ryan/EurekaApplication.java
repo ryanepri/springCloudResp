fb8c18d268b179debccc31644a14640c25ef5566
ryan <807937928@qq.com>
2023-04-14 08:48:55 +0800

    eureka服务注册中心 搭建

---
+++ wfw/eureka/src/main/java/com/ryan/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.ryan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @EnableEurekaServer 代表当前这个springboot项目 作为一个服务注册中心
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
