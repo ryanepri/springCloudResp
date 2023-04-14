a1b2c7c44828213a6646b177d826215b550025d9
ryan <807937928@qq.com>
2023-04-14 16:31:42 +0800

    Eureka单机搭建1-order模块连接eureka服务器中心

---
+++ wfw/order/src/main/java/com/ryan/OrderApplication.java
@@ -0,0 +1,18 @@
+package com.ryan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @EnableEurekaClient
+ * 将该项目作为Eureka的客户端
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
